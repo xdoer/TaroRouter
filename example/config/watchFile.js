91e188e68869bf642628cc725c812edcd2a6eebb
Thomas Trainset <gotoanything@foxmail.com>
2021-01-03 21:42:37 +0800

    fix: listen multiple times

--- example/config/watchFile.js
+++ example/config/watchFile.js
@@ -1,59 +1,43 @@
-const path = require('path')
-const shell = require('shelljs')
+const path = require('path');
+const shell = require('shelljs');
 
-const basePath = path.resolve(__dirname, '../src')
+const basePath = path.resolve(__dirname, '../src');
 
 function execGenerated(log) {
-  console.log(log)
-  shell.exec('npm run gen')
+  console.log(log);
+  shell.exec('npm run gen');
 }
 
-const fileAction = {
-  on: {
-    add: (_, path) => {
-      execGenerated(`File ${path} has been added`)
-    },
-    unlink: (_, path) => {
-      execGenerated(`File ${path} has been remove`)
-    },    
-  }
-}
-
-const dirAction = {
-  on: {
-    unlink: (_, path) => {
-      execGenerated(`File ${path} has been remove`)
-    },    
-  }
-}
-
-const opt = { persistent: true, ignoreInitial: true }
+const opt = { persistent: true, ignoreInitial: true };
 
 module.exports = {
   chokidarConfigList: [
     {
-      // 监听主包页面创建和删除
-      file: basePath + '/pages/**/index.tsx',
-      opt,
-      actions: fileAction,
-    },
-    {
-      // 监听主包页面文件夹删除
-      file: basePath + '/pages/**',
-      opt,
-      actions: dirAction,
-    },
-    {
-      // 监听分包页面文件创建和删除
+      // 监听页面文件创建
       file: basePath + '/**/pages/**/index.tsx',
       opt,
-      actions: fileAction,
+      actions: {
+        on: {
+          add: (_, path) => {
+            execGenerated(`页面 ${path} 被添加`);
+          },
+          // unlink: (_, path) => {
+          //   execGenerated(`页面 ${path} 被移除`)
+          // },
+        },
+      },
     },
     {
-      // 监听分包页面文件夹删除
+      // 监听页面文件夹删除
       file: basePath + '/**/pages/**',
       opt,
-      actions: dirAction,
+      actions: {
+        on: {
+          unlink: (_, path) => {
+            execGenerated(`页面 ${path} 被移除`);
+          },
+        },
+      },
     },
   ],
-}
+};
